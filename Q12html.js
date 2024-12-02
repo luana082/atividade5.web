a376d503e956194d84df3619e4d2911fa2fe63f9
luana082 <lls40@aluno.ifal.edu.br>
2024-12-02 11:57:57 -0300

    Q12html.js
    nao consegui terminar

---
+++ Q12html.js
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Média do Aluno</title>
+</head>
+<body>
+    <h1>Programa de Ver Nota dos Alunos</h1>
+    <form>
+        <p>
+            Nome do Aluno:
+            <input type="text" id="nomedoAluno" required>
+        </p>
+    
+    <p>
+        1° Nota
+        <input type="number" min="0" max="10" id="nota1" required>
+    </p>
+    <p>
+        2° Nota
+        <input type="number" min="0" max="10" id="nota2" required>
