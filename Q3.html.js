b12ad51838c08c584b1f4a09679f904bca063595
luana082 <lls40@aluno.ifal.edu.br>
2024-12-02 11:29:32 -0300

    Ques3.html.js
    código html
    código js
    bjo divo

---
+++ Q3.html.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>De boas</title>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+
+
+let dolar = parseFloat(prompt("Digite o valor do dolar para transformar em real: "))
+let real = 1;
+let conversao = (dolar * 6);
+alert(conversao);
