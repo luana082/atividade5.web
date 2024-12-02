2bc66438290eb820f08786774a0dfef739603493
luana082 <lls40@aluno.ifal.edu.br>
2024-12-02 11:59:13 -0300

    Q13html.js
    html.js

---
+++ Q13html.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Média do Aluno</title>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+
+ let numero = parseInt(prompt("Digite um numero:"));
+    if (numero % 2 === 0) {
+      
+        alert('Voce digitou um numero par');
+    } else {
+        alert('Voce digitou um número impar');
+    }
